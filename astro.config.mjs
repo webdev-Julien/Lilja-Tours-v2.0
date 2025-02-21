b9baa7cbcf2efbe4339a1914a5ab99f7e59ef28a
webdev-Julien <julien.achache.is@gmail.com>
2025-02-21 12:20:13 +0000

    again

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://webdev-Julien.github.io'
+  site: 'https://webdev-Julien.github.io',
+  base: "/Lilja-Tours-v2.0/",
+  outDir: "dist"
 });
