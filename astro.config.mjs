87481f8871dd8cbe4636e4fdfb4bb4a90ebc4d8d
webdev-Julien <julien.achache.is@gmail.com>
2025-02-21 12:05:08 +0000

    deployagain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-  site: 'https://webdev-Julien.github.io'
+  integrations: [mdx()]
 });
